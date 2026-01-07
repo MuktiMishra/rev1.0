f9d26e1acecc17abfe13a7cd98a2d0042260d8e7
mukti <muktimishra0495@gmail.com>
2026-01-07 14:21:39 +0530

    sorted numbers and writing in output.txt using utf-8

---
+++ javascript_imp_basics/sortNumber/index.js
@@ -0,0 +1,33 @@
+// write code to sort numbers in ascending order from file input1.txt and input2.txt
+// and write the sorted output to output.txt
+const fs = require('fs');
+const path = require('path');
+const inputFile1 = path.join(__dirname, 'data', 'input1.txt');
+const inputFile2 = path.join(__dirname, 'data', 'input2.txt');
+const outputFile = path.join(__dirname, 'data', 'output.txt');  
+
+
+// Read numbers from input files
+
+const readNumbersFromFile = (filePath) => {
+    const data = fs.readFileSync(filePath, 'utf-8');
+
+    return data
+        .split(/[,\s]+/)     // commas + spaces + newlines
+        .filter(Boolean)
+        .map(str => Number(str.trim()))
+        .filter(num => !isNaN(num));
+};
+
+
+const numbers1 = readNumbersFromFile(inputFile1);
+const numbers2 = readNumbersFromFile(inputFile2);
+// Merge the two arrays
+const mergedNumbers = [...numbers1, ...numbers2];
+
+// Sort the numbers in ascending order
+mergedNumbers.sort((a, b) => a - b);
+
+// Write the sorted numbers to the output file
+fs.writeFileSync(outputFile, mergedNumbers.join('\n'));
+console.log('Sorted numbers written to', outputFile);
