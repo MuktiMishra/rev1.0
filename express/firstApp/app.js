63881c237dc03021b61f01c23bd850df8140a184
mukti <muktimishra0495@gmail.com>
2026-01-08 10:09:20 +0530

    backend rev started -- express

--- express/firstApp/app.js
+++ express/firstApp/app.js
@@ -1,18 +1,3 @@
-// // differnce betwwen library and framework
-// // A library is a collection of functions and tools that you can call upon to perform specific tasks,
-// //  while a framework provides a structured way to build applications, often dictating the 
-// // architecture and flow of the application.
-// //library -> u can make changes
-// //framework -> you have to follow the rules
-
-// //React is a library or a framework?
-// // React is considered a library because it primarily focuses on building user interfaces
-
-
-// // What are routes?
-// // Routes are the endpoints defined in a web application that determine how the application 
-// // responds to client requests.
-// // They map specific URLs to functions that handle the requests and generate responses.
 const express = require('express');
 
 const app = express();
@@ -26,9 +11,25 @@ app.get('/about', (req, res) => {
     res.send("This is the about page");
 });
 
+app.get('/products/:id/:name', (req , res)=>{
+    res.send("Product id is in the url "+ req.params.id + " and name is " + req.params.name);
+}) // dynamic route
+
+app.get('/user/:name/:age/:city',(req , res)=>{
+    // same as 
+    //let name = req.params.name;
+    //let age = req.params.age;
+    //let city = req.params.city;
+    let {name , age , city}= req.params;
+    res.send(`User name is ${name} , age is ${age} and city is ${city}`);
+}) // dynamic route with destructuring
+
 app.use((req, res) => {
     res.status(404).send("Crazyyy Kiya Reee");
-}); 
+});  // middleware to handle 404 errors // if no route matches this is executed
+// written at the end after all routes
+
+
 
 // Start server
 app.listen(4000, () => {
