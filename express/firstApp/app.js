73bb71baa952d3be7a9ae34beffa31efe86ba19c
mukti <muktimishra0495@gmail.com>
2026-01-09 11:59:05 +0530

    server side rendering , client side rendering and sending html file via express

--- express/firstApp/app.js
+++ express/firstApp/app.js
@@ -4,8 +4,47 @@ const app = express();
 
 // Route
 app.get('/', (req, res) => {
+    console.log(req.query); // to get query parameters
     res.send("Hello from Express JS");
 });
+
+let movies = [
+    {name: "Inception", director: "Christopher Nolan"},
+    {name: "Interstellar", director: "Mukti Mishra"},
+    {name: "The Dark Knight", director: "Christopher Nolan"},
+    {name:"kdf", director:"sdf"}
+];
+app.get('/movies', (req, res) => {
+    let name = req.query.name;
+
+    if (name) {
+        let movie = movies.find(
+            m => m.name.toLowerCase() === name.toLowerCase()
+        );
+
+        if (movie) {
+            res.send("Director of " + name + " is " + movie.director);
+        } else {
+            res.send("Movie not found");
+        }
+    } else {
+        res.send("Please provide movie name");
+    }
+});
+
+// csr -> client side rendering , in this only data is sent from server to client , use api to send 
+// data to client 
+// ssr -> server side rendering , in this whole html page is sent from server to client
+
+app.get('/temp', (req, res) => {
+    res.send(`<h1>This is the temp page</h1>`);
+
+});
+
+app.get('/file',(req, res)=>{
+    res.sendFile(__dirname + '/index.html'); // to send html file from server to client
+})
+
 
 app.get('/about', (req, res) => {
     res.send("This is the about page");
--- javascript_imp_basics/script.js
+++ javascript_imp_basics/script.js
@@ -1,3 +1,4 @@
+//namste JS; from yt
 console.log(x);
 var x = 10;
 console.log(x);
@@ -24,11 +25,15 @@ for(var i=0; i<10; i++){
 
 }
 
+// if we use var instead of let in the for loop, the output will be 10 printed 10 times after 4 seconds
+// This is because 'var' is function-scoped, so by the time the callbacks execute, the loop has completed 
+// and 'i' has the value 10. All callbacks reference the same 'i'.
+
 // It is not 4 × 10 = 40 seconds because setTimeout does not pause or wait; it only schedules a task. 
 // The for loop executes instantly and registers all 10 timers almost at the same time, each with 
 // the same 4-second delay. All these timers then wait in parallel, and once 4 seconds are over, 
 // their callbacks become ready together and are executed by the event loop. Since the delays run
-//  concurrently and not one after another, the total time to get the full output is about 4 seconds, 
-//  not 40 seconds.
+// concurrently and not one after another, the total time to get the full output is about 4 seconds, 
+// not 40 seconds.
 
 console.log("hello world");
