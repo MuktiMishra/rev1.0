9783e61557df712ca62b9ac5ba9577bdc107e5a2
mukti <muktimishra0495@gmail.com>
2026-01-14 23:12:19 +0530

    json.stringify() and json.parse() usage

--- javascript_imp_basics/js_architecture/script1.js
+++ javascript_imp_basics/js_architecture/script1.js
@@ -8,8 +8,25 @@ document.getElementById('myButton').addEventListener('click', function() {
 
 var a = {
     name : 'Mukti',
-    age : 25,
+    age : 20,
     greet: function() {
         console.log('Hello, ' + this.name);
     }
 }
+let x=3;
+function mfn(){
+   let x=2;
+   console.log(x);
+   console.log(x.this); // undefined
+}
+mfn();
+console.log(x);
+
+const obj = {
+    name:"mukti",
+    age:20
+}
+
+Object.freeze(obj);  
+obj.age = 21; // This will not change the age property
+console.log(obj.age); // Outputs: 20
